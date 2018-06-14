36daba0dceb3a0c9c392b058a36fec4ae9fb219a
leihuang <leihuang@ecarx.com.cn>
2018-06-14 17:59:40 +0800

    feat:读取配置文件操作
    Signed-off-by: leihuang <leihuang@ecarx.com.cn>

--- src/main/java/com/haley/spring0to1/BeanConfiguration.java
+++ src/main/java/com/haley/spring0to1/BeanConfiguration.java
@@ -1,12 +1,23 @@
 package com.haley.spring0to1;
 
+import com.haley.spring0to1.domain.model.DataSourceProperties;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
 
 @Configuration
 public class BeanConfiguration {
+
+    //可以通过@Value直接读取properties里面的键值
+    @Value("${ds.userName}")
+    private String userName;
+
+    @Autowired
+    private Environment environment;
 
     @Bean
     @Profile("dev")
@@ -29,6 +40,8 @@ public class BeanConfiguration {
             public void run() {
                 LoggerFactory.getLogger(BeanConfiguration.class).warn("测试环境使用的 Bean");
                 System.out.println("测试环境使用的 Bean");
+                System.out.print("userName:"+userName);
+                System.out.print("userPassword:"+environment.getProperty("ds.password"));
             }
         };
     }
--- src/main/java/com/haley/spring0to1/SpringbootApplication.java
+++ src/main/java/com/haley/spring0to1/SpringbootApplication.java
@@ -1,8 +1,10 @@
 package com.haley.spring0to1;
 
+import com.haley.spring0to1.domain.model.DataSourceProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,6 +14,7 @@ import org.springframework.context.annotation.Configuration;
 @ComponentScan
 public class SpringbootApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringbootApplication.class, args);
+        ConfigurableApplicationContext context =SpringApplication.run(SpringbootApplication.class, args);
+        context.getBean(DataSourceProperties.class).show();
     }
 }
