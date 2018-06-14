b0a4d51dc6b4be36f2ac757d9a7c82fcf99402e7
leihuang <leihuang@ecarx.com.cn>
2018-06-14 15:46:16 +0800

    feat:热部署
    Signed-off-by: leihuang <leihuang@ecarx.com.cn>

--- src/main/java/com/haley/spring0to1/SpringbootApplication.java
+++ src/main/java/com/haley/spring0to1/SpringbootApplication.java
@@ -1,9 +1,15 @@
 package com.haley.spring0to1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
 public class SpringbootApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringbootApplication.class, args);
