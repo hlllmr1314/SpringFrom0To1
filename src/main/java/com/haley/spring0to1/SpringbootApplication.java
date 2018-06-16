8c45c1501b38cc65548f8879e41381d47b57b860
leihuang <leihuang@ecarx.com.cn>
2018-06-16 17:09:35 +0800

    feat:修改banner
    Signed-off-by: leihuang <leihuang@ecarx.com.cn>

--- src/main/java/com/haley/spring0to1/SpringbootApplication.java
+++ src/main/java/com/haley/spring0to1/SpringbootApplication.java
@@ -1,10 +1,12 @@
 package com.haley.spring0to1;
 
 import com.haley.spring0to1.domain.model.DataSourceProperties;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,9 +14,16 @@ import org.springframework.context.annotation.Configuration;
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        ConfigurableApplicationContext context =SpringApplication.run(SpringbootApplication.class, args);
+        SpringApplication application = new SpringApplication(SpringBootApplication.class);
+
+//        application.setBannerMode(Banner.Mode.OFF);// 关闭启动Banner
+        application.setBannerMode(Banner.Mode.CONSOLE);// 输出Banner到控制台
+//        application.setBannerMode(Banner.Mode.LOG);// 输出Banner到日志中
+
+        ApplicationContext context = application.run(args);
         context.getBean(DataSourceProperties.class).show();
+
     }
 }
